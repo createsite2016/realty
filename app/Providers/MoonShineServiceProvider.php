76962ed22a1688dd658ab32767a5c7bab7bed35e
💻Андрей Галушко <createsite2016@gmail.com>
2023-10-11 19:17:41 +0300

    added property types

--- app/Providers/MoonShineServiceProvider.php
+++ app/Providers/MoonShineServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Country;
 use App\MoonShine\Resources\AmenitieResource;
 use App\MoonShine\Resources\CountryResource;
 use App\MoonShine\Resources\CurrencyResource;
+use App\MoonShine\Resources\PropertyTypeResource;
 use Illuminate\Support\ServiceProvider;
 use MoonShine\MoonShine;
 use MoonShine\Menu\MenuItem;
@@ -22,6 +23,7 @@ class MoonShineServiceProvider extends ServiceProvider
             MenuItem::make('Удобства', new AmenitieResource()),
             MenuItem::make('Страна', new CountryResource()),
             MenuItem::make('Валюта', new CurrencyResource()),
+            MenuItem::make('Тип недвижимости', new PropertyTypeResource()),
         ]);
     }
 }
