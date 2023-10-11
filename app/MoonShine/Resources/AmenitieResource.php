6971ddac01022231ef56c51329d9950739aaace3
💻Андрей Галушко <createsite2016@gmail.com>
2023-10-11 18:44:15 +0300

    added amenity

---
+++ app/MoonShine/Resources/AmenitieResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Amenitie;
+
+use MoonShine\Fields\Text;
+use MoonShine\Resources\Resource;
+use MoonShine\Fields\ID;
+use MoonShine\Actions\FiltersAction;
+
+class AmenitieResource extends Resource
+{
+	public static string $model = Amenitie::class;
+
+	public static string $title = 'Удобства';
+
+	public function fields(): array
+	{
+		return [
+		    ID::make()->sortable(),
+            Text::make('Название удобства', 'amenity_name'),
+        ];
+	}
+
+	public function rules(Model $item): array
+	{
+	    return [];
+    }
+
+    public function search(): array
+    {
+        return ['id'];
+    }
+
+    public function filters(): array
+    {
+        return [];
+    }
+
+    public function actions(): array
+    {
+        return [
+            FiltersAction::make(trans('moonshine::ui.filters')),
+        ];
+    }
+}
--- app/Providers/MoonShineServiceProvider.php
+++ app/Providers/MoonShineServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
+use App\MoonShine\Resources\AmenitieResource;
 use Illuminate\Support\ServiceProvider;
 use MoonShine\MoonShine;
-use MoonShine\Menu\MenuGroup;
 use MoonShine\Menu\MenuItem;
 use MoonShine\Resources\MoonShineUserResource;
 use MoonShine\Resources\MoonShineUserRoleResource;
@@ -14,17 +14,9 @@ class MoonShineServiceProvider extends ServiceProvider
     public function boot(): void
     {
         app(MoonShine::class)->menu([
-            MenuGroup::make('moonshine::ui.resource.system', [
-                MenuItem::make('moonshine::ui.resource.admins_title', new MoonShineUserResource())
-                    ->translatable()
-                    ->icon('users'),
-                MenuItem::make('moonshine::ui.resource.role_title', new MoonShineUserRoleResource())
-                    ->translatable()
-                    ->icon('bookmark'),
-            ])->translatable(),
-
-            MenuItem::make('Documentation', 'https://laravel.com')
-                ->badge(fn() => 'Check'),
+            MenuItem::make('Пользователи', new MoonShineUserResource()),
+            MenuItem::make('Роли', new MoonShineUserRoleResource()),
+            MenuItem::make('Удобства', new AmenitieResource()),
         ]);
     }
 }
